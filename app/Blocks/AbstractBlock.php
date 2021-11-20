2f3cce9ccc38b91c46318929ed668c31a3a116fd
Pascal GAULT <pascal@inrage.fr>
2021-11-20 14:17:48 +0100

    feat(blocks): add default configuration to blocks

--- app/Blocks/AbstractBlock.php
+++ app/Blocks/AbstractBlock.php
@@ -7,10 +7,34 @@ use function Roots\asset;
 
 abstract class AbstractBlock extends Block
 {
+    public $supports = [
+        'align' => true,
+        'align_text' => false,
+        'align_content' => false,
+        'full_height' => false,
+        'anchor' => false,
+        'mode' => false,
+        'multiple' => true,
+        'jsx' => true,
+    ];
+
+    public $mode = 'preview';
+    public $parent = [];
+    public $post_types = [];
+    public $keywords = [];
+    public $align = '';
+    public $align_text = '';
+    public $align_content = '';
+
     public function enqueue(): void
     {
         if (asset('styles/blocks/' . $this->slug . '.css')->exists()) {
-            wp_enqueue_style('blocks/' . $this->slug . '.css', asset('styles/blocks/' . $this->slug . '.css')->uri(), false, null);
+            wp_enqueue_style(
+                'blocks/' . $this->slug . '.css',
+                asset('styles/blocks/' . $this->slug . '.css')->uri(),
+                false,
+                null
+            );
         }
     }
 
