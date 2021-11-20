a1418e2bbde82b54a1e15ceb992035f943b221d4
Pascal GAULT <pascal@inrage.fr>
2021-11-20 14:18:10 +0100

    fix(block/container): extends from AbstractBlock

--- app/Blocks/Container.php
+++ app/Blocks/Container.php
@@ -6,21 +6,20 @@ use Log1x\AcfComposer\Block;
 use StoutLogic\AcfBuilder\FieldsBuilder;
 use function Roots\asset;
 
-class Container extends Block
+class Container extends AbstractBlock
 {
     public $name = 'Container (1200px)';
     public $slug = 'container';
     public $description = "Création d'un container qui utilise le max-width du site et centre ses enfants";
     public $category = 'theme';
     public $icon = 'align-center';
-    public $mode = 'preview';
 
-    public function with()
+    public function with(): array
     {
         return [];
     }
 
-    public function fields()
+    public function fields(): array
     {
         return [];
     }
