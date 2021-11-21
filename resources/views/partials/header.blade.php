133cf8e571d2f10ad0c517640244d4b9bc107bbd
Pascal GAULT <pascal@inrage.fr>
2021-11-21 18:19:54 +0100

    style(nav): add level 0 of the desk primary menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,7 +14,7 @@
         <div class="header__menu">
           @if (has_nav_menu('primary_navigation'))
             <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-              {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+              {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false, 'container' => false]) !!}
             </nav>
           @endif
 
