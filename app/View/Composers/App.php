1bd546da429b1f8783e4b1591f88e991071d6dde
Pascal GAULT <pascal@inrage.fr>
2021-12-12 11:08:13 +0100

    fix(contact): apply the Bud way to inject CSS

--- app/View/Composers/App.php
+++ app/View/Composers/App.php
@@ -3,7 +3,9 @@
 namespace App\View\Composers;
 
 use Roots\Acorn\View\Composer;
+
 use function Roots\asset;
+use function Roots\bundle;
 
 class App extends Composer
 {
@@ -13,10 +15,10 @@ class App extends Composer
 
     public function with(): array
     {
-        if(has_shortcode(get_the_content(), 'contact-form-7')) {
-            wp_enqueue_style('blocks/contact-form.css', asset('styles/blocks/contact-form.css')->uri(), false, null);
+        if (has_shortcode(get_the_content(), 'contact-form-7')) {
+            bundle('contact-form')->enqueueCss();
 
-            if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
+            if (function_exists('wpcf7_enqueue_scripts')) {
                 wpcf7_enqueue_scripts();
             }
         }
